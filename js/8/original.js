1ce390d270fb860bdd46d54f0daee9b8e190133d
beppo-sturmtief <beppo.sturmtief@gmail.com>
2022-12-09 17:14:41 +0100

    fixed version of Day 8 - first counted each tree once from each direction

--- js/8/original.js
+++ js/8/original.js
@@ -1,14 +1,37 @@
-const input = (await (await fetch('https://adventofcode.com/2022/day/8/input')).text()).trim();
-const grid = input.split('\n').map(s=>s.split().map(d=>parseInt(d)));
-const rotatedGrid = grid[0].map((_,i)=>grid.reduce((col,row)=>[...col, row[i]], []));
-
-const evaluateTreeVisibility = ({count, highest}, height) => (height>highest) ? {count: count+1, highest: height} : {count, highest};
 const plus = (a,b) => a+b;
+const rotate = grid => grid[0].map((_,i)=>grid.reduce((col,row)=>[...col, row[i]], []));
+const input = (await (await fetch('https://adventofcode.com/2022/day/8/input')).text()).trim();
+const grid = input.split('\n').map(s=>s.split('').map(d=>parseInt(d)));
 
-const visibleFromNorth = grid.map(col => col.reduce(evaluateTreeVisibility, {count: 0, highest:-1}).count).reduce(plus, 0);
-const visibleFromSouth = grid.map(col => col.reduceRight(evaluateTreeVisibility, {count: 0, highest:-1}).count).reduce(plus, 0);
-const visibleFromWest = rotatedGrid.map(row => row.reduce(evaluateTreeVisibility, {count: 0, highest:-1}).count).reduce(plus, 0);
-const visibleFromEast = rotatedGrid.map(row => row.reduceRight(evaluateTreeVisibility, {count: 0, highest:-1}).count).reduce(plus, 0);
-const visible = visibleFromNorth + visibleFromSouth + visibleFromWest + visibleFromEast;
+const evaluateTreeVisibility = ({visible, highest}, height) => (height>highest) ? {visible: [...visible, 1], highest: height} : {visible: [...visible, 0], highest};
 
-console.log(`Part 1: ${visible} - Part2: ${-1}`);
+const rotatedGrid = rotate(grid);
+
+const visibleFromNorthGrid = grid.map(col => col.reduce(evaluateTreeVisibility, {visible: [], highest:-1}).visible);
+const visibleFromSouthGrid = grid.map(col => col.reduceRight(evaluateTreeVisibility, {visible: [], highest:-1}).visible.reverse());
+const visibleFromWestRotatedGrid = rotatedGrid.map(row => row.reduce(evaluateTreeVisibility, {visible: [], highest:-1}).visible);
+const visibleFromEastRotatedGrid = rotatedGrid.map(row => row.reduceRight(evaluateTreeVisibility, {visible: [], highest:-1}).visible.reverse());
+const visible = visibleFromNorthGrid
+  .map((row, ri) => row
+       .reduce((count, val, ci) => count + 
+               ((val + visibleFromSouthGrid[ri][ci] + visibleFromWestRotatedGrid[ci][ri] + visibleFromEastRotatedGrid[ci][ri]) > 0
+               ? 1 
+               : 0)))
+  .reduce(plus, 0);
+
+const countPrecedingVisibleTrees = ({counts, countsPerHeight}, height) => ({
+  counts: [...counts, countsPerHeight[height]],
+  countsPerHeight: countsPerHeight.map((count,i) => i<=height ? 1 : (count+1))});
+
+const visibilityToNorthGrid = grid.map(col => col.reduce(countPrecedingVisibleTrees, {counts:[], countsPerHeight: [0,0,0,0,0,0,0,0,0,0]}).counts);
+const visibilityToSouthGrid = grid.map(col => col.reduceRight(countPrecedingVisibleTrees, {counts:[], countsPerHeight: [0,0,0,0,0,0,0,0,0,0]}).counts.reverse());
+const visibilityToWestRotatedGrid = rotatedGrid.map(row => row.reduce(countPrecedingVisibleTrees, {counts:[], countsPerHeight: [0,0,0,0,0,0,0,0,0,0]}).counts);
+const visibilityToEastRotatedGrid = rotatedGrid.map(row => row.reduceRight(countPrecedingVisibleTrees, {counts:[], countsPerHeight: [0,0,0,0,0,0,0,0,0,0]}).counts.reverse());
+const maxScenicValue = visibilityToNorthGrid
+  .map((row, ri) => row
+       .reduce((max, val, ci) => 
+               Math.max(max, val * visibilityToSouthGrid[ri][ci] * visibilityToWestRotatedGrid[ci][ri] * visibilityToEastRotatedGrid[ci][ri]),
+               0))
+  .reduce((max,v)=>v>max?v:max, 0)
+
+console.log(`Part 1: ${visible} - Part2: ${maxScenicValue}`);
