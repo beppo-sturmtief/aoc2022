7f16049b7d9b6ca563a11e95d2424c0b0d9e0729
beppo-sturmtief <beppo.sturmtief@gmail.com>
2022-12-11 21:04:29 +0100

    Add solution Part 2 of day 11 - same is not applicable for part 1, but unified a bit

--- js/11/original.js
+++ js/11/original.js
@@ -1,51 +1,66 @@
 const parseMonkeyDesc = monkeyDesc => {
-	const rows = monkeyDesc.split('\n');
+  const rows = monkeyDesc.split('\n');
   const opParts = rows[2].split(' ').slice(-3)
-  const opValue1 = parseInt(opParts[0]);
-  const opValue2 = parseInt(opParts[2]);
+  const opValue1 = opParts[0] == 'old' ? false : parseInt(opParts[0]);
+  const opValue2 = opParts[2] == 'old' ? false : parseInt(opParts[2]);
   const operation = (opParts[1] == '*')
     ? old => (opValue1 || old) * (opValue2 || old)
     : old => (opValue1 || old) + (opValue2 || old);
   const conditionValue = parseInt(rows[3].split(' ').slice(-1)[0]);
   const trueMonkey = parseInt(rows[4].split(' ').slice(-1)[0]);
   const falseMonkey = parseInt(rows[5].split(' ').slice(-1)[0]);
-  const recievingMonkey = worry => (worry % conditionValue == 0 ? trueMonkey : falseMonkey);
+  const recievingMonkey = worry => worry % conditionValue == 0 ? trueMonkey : falseMonkey;
   
   return {
     number: parseInt(rows[0].split(' ')[1]),
-    items: rows[1].split(/,? +/).slice(3).map(s=>BigInt(s)),
+    initialItems: rows[1].split(/,? +/).slice(3).map(s=>parseInt(s)),
     operation,
     recievingMonkey,
-		itemsInspected: 0,
+    conditionValue
   };
 }
 const input = (await (await fetch('https://adventofcode.com/2022/day/11/input')).text()).trim();
 const monkeys = input.split('\n\n').map(parseMonkeyDesc);
 
+const perMonkeyDataPart1 = monkeys.map(monkey => ({
+  itemsInspected: 0,
+  itemWorries: [...monkey.initialItems]
+}));
+
 for (let round = 0; round < 20; round++) {
-	for (monkey of monkeys) {
-		while (monkey.items.length > 0) {
-			monkey.itemsInspected++;
-      const itemWorry = Math.floor(monkey.operation(monkey.items.shift()) / 3);
-      monkeys[monkey.recievingMonkey(itemWorry)].items.push(itemWorry);
+  for (monkey of monkeys) {
+    const monkeyData = perMonkeyDataPart1[monkey.number];
+    while (monkeyData.itemWorries.length > 0) {
+      monkeyData.itemsInspected++;
+      const itemWorries = Math.floor(monkey.operation(monkeyData.itemWorries.shift()) / 3);
+      perMonkeyDataPart1[monkey.recievingMonkey(itemWorries)].itemWorries.push(itemWorries);
     }
   }
 }
 
-const monkeyBusiness = monkeys.map(m => m.itemsInspected).sort((a,b)=>b-a).slice(0,2).reduce((a,b)=>a*b, 1);
+const monkeyBusiness = perMonkeyDataPart1.map(m => m.itemsInspected).sort((a,b)=>b-a).slice(0,2).reduce((a,b)=>a*b, 1);
 
-const monkeysForPart2 = input.split('\n\n').map(parseMonkeyDesc);
+
+const conditionDivisors = [...new Set(monkeys.map(m=>m.conditionValue).sort((a,b)=>a-b))];
+const perMonkeyDataPart2 = monkeys.map(monkey => ({
+  itemsInspected: 0,
+  itemWorries: monkey
+	.initialItems
+	.map(item => conditionDivisors
+	     .map(divisor => item % divisor))
+}));
 
 for (let round = 0; round < 10000; round++) {
-	for (monkey of monkeysForPart2) {
-		while (monkey.items.length > 0) {
-			monkey.itemsInspected++;
-      const itemWorry = monkey.operation(monkey.items.shift());
-      monkeysForPart2[monkey.recievingMonkey(itemWorry)].items.push(itemWorry);
+  for (monkey of monkeys) {
+    const monkeyData = perMonkeyDataPart2[monkey.number];
+    while (monkeyData.itemWorries.length > 0) {
+      monkeyData.itemsInspected++;
+      const itemWorries = monkeyData.itemWorries.shift().map((worry, i) => monkey.operation(worry) % conditionDivisors[i]);
+      perMonkeyDataPart2[monkey.recievingMonkey(itemWorries[conditionDivisors.indexOf(monkey.conditionValue)])].itemWorries.push(itemWorries);
     }
   }
 }
 
-const monkeyBusinessPart2 = monkeysForPart2.map(m => m.itemsInspected).sort((a,b)=>b-a).slice(0,2).reduce((a,b)=>a*b, 1);
+const monkeyBusinessPart2 = perMonkeyDataPart2.map(m => m.itemsInspected).sort((a,b)=>b-a).slice(0,2).reduce((a,b)=>a*b, 1);
 
 console.log(`Part 1: ${monkeyBusiness} - Part 2: ${monkeyBusinessPart2}`);
