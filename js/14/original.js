85d37aac29310a7e5d475672af8abcec35524252
beppo-sturmtief <beppo.sturmtief@gmail.com>
2022-12-18 15:55:40 +0100

    day 14 now working

--- js/14/original.js
+++ js/14/original.js
@@ -1,78 +1,69 @@
+const oneMove = (field, width, pos, startPos) => {
+  let move = 0;
+  if (field[pos +  width] == '.')
+    move = width;
+  else if (field[pos + width - 1] == '.')
+    move = width - 1;
+  else if (field[pos +  width + 1] == '.')
+    move = width + 1;
+  else
+    move = startPos - pos;
+  
+  if (move > 0)
+    field[pos] = '.';
+
+  field[pos + move] = 'o';
+  return pos + move;
+};
+
 const input = (await (await fetch('https://adventofcode.com/2022/day/14/input')).text()).trim();
 const lines = input.split('\n').map(line => line.split(' -> ').map(pair => pair.split(',').map(i => parseInt(i))));
-const bounds = lines.flatMap(x => x).reduce(([minX,maxX, minY, maxY], pair) => 
-                                            [Math.min(minX, pair[0]), Math.max(maxX, pair[0]),
-                                             Math.min(minY, pair[1]), Math.max(maxY, pair[1])], 
-                                            [Infinity, -Infinity, 0, -Infinity]);
-const width = bounds[1] - bounds[0] + 1 + 2;
-const height = bounds[3] - bounds[2] + 1 + 1;
+const maxY = lines.flatMap(l=>l).reduce((max, [x,y]) => Math.max(max,y), 0);
+const width = 1001;
+const height = maxY + 3;
 const pos = ([x,y]) => x + width * y;
-const origPos = ([x,y]) => (x - bounds[0] + 1) + width * (y - bounds[2]);
 
 let field = Array(width * height).fill('.');
 
 for (let line of lines) {
   for (let idx = 0; idx < line.length - 1; idx++) {
-    const begin = Math.min(origPos(line[idx]), origPos(line[idx+1]));
-    const end = Math.max(origPos(line[idx]), origPos(line[idx+1]));
+    const begin = Math.min(pos(line[idx]), pos(line[idx+1]));
+    const end = Math.max(pos(line[idx]), pos(line[idx+1]));
     const step = (line[idx][0] != line[idx+1][0] ? 1 : width);
     for (let pos = begin; pos <= end; pos += step) field[pos] = '#';
   }
 }
 
-const startPos = origPos([500,0]);
+for (let pos = width * (height - 1); pos < width * height; pos++)
+  field[pos] = 'X';
+
+const startPos = pos([500,0]);
 
 let sandPos = startPos;
 field[sandPos] = 'o';
 let sandCount = 1;
 
-while (sandPos < (height-1) * width) {
-  
-  let move = 0;
-  if (field[sandPos +  width] == '.')
-    move = width;
-  else if (field[sandPos + width - 1] == '.')
-    move = width - 1;
-  else if (field[sandPos +  width + 1] == '.')
-    move = width + 1;
-  else if (sandPos != startPos) {
-    move = startPos - sandPos;
+while (sandPos < (height-2) * width) {
+  sandPos = oneMove(field, width, sandPos, startPos);
+  if (sandPos == startPos)
     sandCount++;
-  } else {
-    console.log('completely filled...');
-    break;
-  }
-  if (move > 0)
-    field[sandPos] = '.';
-  sandPos += move;
-  field[sandPos] = 'o';
 }
 
-const restingSandCount = sandCount -1;
+const restingSandCount1 = sandCount - 1;
 
 /// part 2
-field = field.concat(Array(width).fill('X'));
-
-while (sandPos < height * width) {
-  let move = 0;
-  if (field[sandPos +  width] == '.')
-    move = width;
-  else if (field[sandPos + width - 1] == '.')
-    move = width - 1;
-  else if (field[sandPos +  width + 1] == '.')
-    move = width + 1;
-  else if (sandPos != startPos) {
-    move = startPos - sandPos;
-    sandCount++;
-  } else {
-    console.log('completely filled...');
+while (true) {
+  const newSandPos = oneMove(field, width, sandPos, startPos);
+  if (newSandPos == sandPos)
     break;
-  }
-  if (move > 0)
-    field[sandPos] = '.';
-  sandPos += move;
-  field[sandPos] = 'o';
+  sandPos = newSandPos;
+  if (sandPos == startPos)
+    sandCount++;
 }
 
 const restingSandCount2 = sandCount;
-console.log(`Part 1: ${restingSandCount} - Part 2: ${restingSandCount2}`);
+console.log(`Part 1: ${restingSandCount1} - Part 2: ${restingSandCount2}`);
+
+// for (let row = 0; row < height; row++) console.log(field.slice(row*width, (row+1)*width).join(''));
+// for (let row = 0; row < height; row++) console.log(field.slice(row*width+450, row*width+700).join(''));
+
